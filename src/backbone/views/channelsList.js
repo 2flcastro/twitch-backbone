6d6429825b982d94dca26dce684ca3beb0901368
Flavio Castro <2flcastro@gmail.com>
2017-05-13 01:15:27 -0700

    Update channelList view

--- src/backbone/views/channelsList.js
+++ src/backbone/views/channelsList.js
@@ -1,21 +1,14 @@
 var $ = require('jquery');
 var Backbone = require('backbone');
-var Handlebars = require('handlebars');
-var fs = require('fs');
 
 var ChannelView = require('./channel');
 var ChannelModel = require('../models/channel');
 
-var template = fs.readFileSync(__dirname + '/../templates/channelsList.hbs', { encoding: 'utf8' });
-
 module.exports = Backbone.View.extend({
+  // Will render a <div> with class "channel-list" as view container
   className: 'channels-list',
 
-  template: Handlebars.compile(template),
-
   render: function() {
-    this.$el.html(this.template());
-
     // Create a view for each channel in collection and add to channelList parent view
     var _this = this;
     this.collection.each(function(channel) {
